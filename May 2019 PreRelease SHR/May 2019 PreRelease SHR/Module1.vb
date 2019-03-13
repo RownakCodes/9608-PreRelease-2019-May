2db60228e4cb591a79b2c08aa944d031f3cd4413
RownakCodes <47391823+RownakCodes@users.noreply.github.com>
2019-03-13 21:24:23 +0600

    Update Module1.vb

--- May 2019 PreRelease SHR/May 2019 PreRelease SHR/Module1.vb
+++ May 2019 PreRelease SHR/May 2019 PreRelease SHR/Module1.vb
@@ -1,9 +1,10 @@
-﻿Module Module1
+Module Module1
+
+
+    Dim store(10000), name, email As String
+    Dim StNum As Integer
 
     Sub Main()
-
-        Dim store(10000), name, email As String
-        Dim StNum As Integer
 
         Console.WriteLine("What is the number of students? ")
         StNum = Console.ReadLine
@@ -14,7 +15,6 @@
         For stindex = 0 To StNum - 1
             Console.Write("Enter the name of the Student: ")
             name = Console.ReadLine()
-
             Console.Write("Enter the email of the Student: ")
             email = Console.ReadLine()
 
@@ -23,15 +23,44 @@
 
             store(stindex) = name + "#" + email
         Next
+
         Console.Clear()
         PrintRecords(store, StNum)
         DeleteRecords(store, StNum)
 
-
+        search()
     End Sub
 
 
+    '===================================================================================================================
+    Sub search()
 
+        Dim searchname, content, result As String
+
+        Console.Clear()
+        Console.WriteLine("Enter the name or a part of it of the one you want to find")
+        searchname = Console.ReadLine()
+
+        Console.Write("Name")
+        Console.Write("                    ") ' 20 spaces
+        Console.WriteLine("Contents")
+
+        For searchindex = 0 To StNum - 1
+            content = store(searchindex)
+
+            For position = 1 To Len(content) + 1
+                result = Mid(content, position, Len(searchname))
+
+                If (result = searchname) Then
+                    Print(content)
+                    Exit For
+                End If
+
+            Next
+
+        Next
+
+    End Sub
 
 
     Sub DeleteRecords(ByRef store() As String, ByVal StNum As Integer)
@@ -41,10 +70,13 @@
             Console.WriteLine("Did anyone leave this class? (Yes or No)")
             leave = Console.ReadLine()
 
+            Console.WriteLine("")
+            Console.WriteLine("")
             If (leave = "Yes") Then
                 Console.WriteLine("Enter the name of the student who left: ")
                 StName = Console.ReadLine()
 
+                Console.WriteLine("")
                 Console.WriteLine("Enter the email of the student who left: ")
                 StEmail = Console.ReadLine()
 
@@ -72,11 +104,6 @@
 
 
     End Sub
-
-
-
-
-
 
 
     Sub PrintRecords(ByRef store() As String, StNum As Integer)
@@ -120,5 +147,35 @@
         Next
 
     End Sub
+
+
+    Sub Print(ByVal content As String)
+
+        Dim spaces As Integer
+        Dim letter, addname, addemail As String
+
+
+        addname = ""
+
+
+
+        For hashfinder = 0 To Len(content) - 1
+            letter = content(hashfinder)
+            If (letter = "#") Then
+                addemail = Mid(content, hashfinder + 2, Len(content))
+                spaces = Len(content)
+                Console.Write(addname)
+                For printspaces = 1 To (24 - Len(addname))
+                    Console.Write(" ")
+                Next
+                Console.WriteLine(addemail)
+                Exit For
+            Else
+                addname = addname + letter
+            End If
+
+        Next
+    End Sub
+
 
 End Module
